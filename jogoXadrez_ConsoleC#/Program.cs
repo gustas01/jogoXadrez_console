b54a6880f015659315ef621e15f8e875fa1faab9
gustas01 <gustasboyevolution@hotmail.com>
2023-05-03 13:27:22 -0300

    criando classe tabuleiro e renomenando solução e projeto

--- jogoXadrez_ConsoleC#/Program.cs
+++ jogoXadrez_ConsoleC#/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+
+Posicao P = new Posicao(3, 4);
+
+Console.WriteLine(P);
---
+++ jogoXadrez_ConsoleC#/tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro {
+  internal class Posicao {
+    public int row;
+    public int column;
+
+    public Posicao(int row, int column) {
+      this.row = row;
+      this.column = column;
+    }
+
+    public override string ToString() {
+      return row + ", " + column;
+    }
+  }
+}
